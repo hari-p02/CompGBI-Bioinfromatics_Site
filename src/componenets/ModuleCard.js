e7c61fa38442c31e30a393d39146e779adca6519
hari-p02 <hari.patchigolla@uconn.edu>
2023-06-26 01:00:36 -0700

    added the module cards with the proper styling

--- src/componenets/ModuleCard.js
+++ src/componenets/ModuleCard.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import "../styles/componenet_styles/ModuleCard.css"
-const ModuleCard = () => {
+const ModuleCard = ( {moduleNum, title, duration, highlight, bgkfrom, bkgto, desc}) => {
+const parts = desc.split(/(<span>|<\/span>)/);
+console.log("linear-gradient(135deg," + bgkfrom + " 0%, " + bkgto + " 100%)");
   return (
-    <div class="card">
-        <div className='lesson-num'> Module 1</div>
-        <h1>Introduction to Python</h1>
-        <div className='duration'>Duration: 80 mins</div>
+    <div class="card-mod" style={{background: "linear-gradient(135deg," + bgkfrom + " 0%, " + bkgto + " 100%)"}}>
+        <div className='top-content'>
+            <div className='lesson-num'> {"Module " + moduleNum}</div>
+            <h1>{title}</h1>
+            <div className='duration'>{"Duration: " +  duration + " mins"}</div>
+        </div>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className='highlight'>sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiat nulla pariatur. Excepteur sint <span className='highlight'>occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt</span> mollit anim id est laborum. 
+            {parts.map((part, index) => {
+                if (part === "<span>") {
+                return (
+                    <span key={index} style={{backgroundColor: highlight, padding: "1.5px", borderRadius: "5px"}}>
+                    {parts[index + 1]}
+                    </span>
+                );
+                } else if (part !== "</span>") {
+                return <span key={index}>{part}</span>;
+                }
+                return null;
+            })}
         </p>
     </div>
   )
--- src/pages/Modules.js
+++ src/pages/Modules.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../styles/pages_styles/Home.css";
+import "../styles/pages_styles/Modules.css";
 import ModuleCard from '../componenets/ModuleCard';
 const Modules = () => {
   const mods = [
@@ -7,18 +7,78 @@ const Modules = () => {
       moduleNum : "1",
       title: "Introduction to Python",
       duration: "80",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className='highlight'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span className='highlight'>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+      highlight: "#71A9F7",
+      bgkfrom: "#2B86C5",
+      bkgto: "#FF3CAC",
+      desc: "Lorem ipsum dolor sit amet, <span>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
     },
     {
       moduleNum : "2",
       title: "Sequence Analysis",
       duration: "70",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className='highlight'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span className='highlight'>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+      highlight: "#FFE066",
+      bgkfrom: "#ff85ec",
+      bkgto: "#84ff98",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "3",
+      title: "RNA Programming",
+      duration: "70",
+      highlight: "#F25F5C",
+      bgkfrom: "#ebadb6",
+      bkgto: "#aceae1",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "4",
+      title: "Genomic Variants",
+      duration: "70",
+      highlight: "#ECB0E1",
+      bgkfrom: "#ED6B86",
+      bkgto: "#FAB3A9",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "5",
+      title: "Evolution & Phylogenetics",
+      duration: "70",
+      highlight: "#DE6C83",
+      bgkfrom: "#EC0B43",
+      bkgto: "#7AE7C7",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "6",
+      title: "Bioinformatics Applications",
+      duration: "70",
+      highlight: "#74D3AE",
+      bgkfrom: "#678D58",
+      bkgto: "#44FFD2",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "7",
+      title: "Machine Learning with Bioinformatics",
+      duration: "70",
+      highlight: "#2CF6B3",
+      bgkfrom: "#9197AE",
+      bkgto: "#DD0426",
+      desc: "Lorem ipsum dolor sit amet, <span>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
+    },
+    {
+      moduleNum : "8",
+      title: "Bioinformatics Ethics",
+      duration: "70",
+      highlight: "#C6D8FF",
+      bgkfrom: "#D90368",
+      bkgto: "#9046CF",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <span>occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span> mollit anim id est laborum."
     }
   ]
   return (
-    <div>
-        <ModuleCard />
+    <div className='all-mods'>
+        {mods.map(mod => (<ModuleCard moduleNum={mod.moduleNum} title={mod.title} duration={mod.duration} highlight={mod.highlight} bgkfrom={mod.bgkfrom} bkgto={mod.bkgto} desc={mod.desc}/>))}
     </div>
   )
 }
