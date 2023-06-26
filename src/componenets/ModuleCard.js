cca0c743c1025dbaa03a7736e7be1173a481bef3
hari-p02 <hari.patchigolla@uconn.edu>
2023-06-26 01:17:29 -0700

    commiting the rest of the changes for the day, the navbar still needs some fixing

--- src/componenets/ModuleCard.js
+++ src/componenets/ModuleCard.js
@@ -2,11 +2,17 @@ import React from 'react'
 import "../styles/componenet_styles/ModuleCard.css"
 const ModuleCard = ( {moduleNum, title, duration, highlight, bgkfrom, bkgto, desc}) => {
 const parts = desc.split(/(<span>|<\/span>)/);
-console.log("linear-gradient(135deg," + bgkfrom + " 0%, " + bkgto + " 100%)");
+// console.log("linear-gradient(135deg," + bgkfrom + " 0%, " + bkgto + " 100%)");
   return (
     <div class="card-mod" style={{background: "linear-gradient(135deg," + bgkfrom + " 0%, " + bkgto + " 100%)"}}>
         <div className='top-content'>
-            <div className='lesson-num'> {"Module " + moduleNum}</div>
+            <div className='cont'>
+                <div className='lesson-num'> {"Module " + moduleNum}</div>
+                <div class="box">
+                    <div class="arrow right"></div>
+                    <div class="text">Go To Module!</div>
+                </div>
+            </div>
             <h1>{title}</h1>
             <div className='duration'>{"Duration: " +  duration + " mins"}</div>
         </div>
