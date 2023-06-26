42eaaccd66ac4c773824ccb8a9acff888d10b816
hari-p02 <hari.patchigolla@uconn.edu>
2023-06-25 23:19:37 -0700

    simplified the nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './componenets/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router> 
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import "../styles/componenet_styles/Navbar.css"
+
 const Navbar = () => {
   return (
     <div class="container">
         <div class="tabs">
-            <input type="radio" id="radio-1" name="tabs" class="radio-1"/>
-            <label class="tab" for="radio-1">Home</label>
-            <input type="radio" id="radio-2" name="tabs" class="radio-2"/>
-            <label class="tab" for="radio-2">Modules</label>
-            <input type="radio" id="radio-3" name="tabs" class="radio-3"/>
-            <label class="tab" for="radio-3">About</label>
+            <a class="tab link-1" for="radio-1" href="/">Home</a>
+            <a class="tab link-2" for="radio-2" href="/modules">Modules</a>
+            <a class="tab link-3" for="radio-3" href="/about">About</a>
             <span class="glider"></span>
         </div>
     </div>
