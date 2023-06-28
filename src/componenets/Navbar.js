dadc369611915389d0978ca38fdcbe4f4c8d2df0
hari-p02 <hari.patchigolla@uconn.edu>
2023-06-28 15:50:51 -0700

    made navbar changes more efficent by using the useLocation hook in the router-dom, instead of a list of booleans as the state

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "../styles/componenet_styles/Navbar.css";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 const Navbar = () => {
-  const [isClicked, setIsClicked] = useState([false, false, false]);
+  // const [isClicked, setIsClicked] = useState([false, false, false]);
   // const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const location = useLocation();
+  const { pathname } = location;
+  const segments = pathname.split("/");
+  console.log(segments);
   const bkstyle = {
     width: "100px",
     backgroundColor: "#e6eef9",
@@ -16,24 +20,24 @@ const Navbar = () => {
         <Link
           class="tab link-1"
           to="/"
-          style={isClicked[0] ? bkstyle : {}}
-          onClick={() => {
-            setIsClicked(
-              isClicked[0] ? [false, false, false] : [true, false, false]
-            );
-          }}
+          style={segments[1] === "" ? bkstyle : {}}
+          // onClick={() => {
+          //   setIsClicked(
+          //     isClicked[0] ? [false, false, false] : [true, false, false]
+          //   );
+          // }}
         >
           Home
         </Link>
         <Link
           class="tab link-2"
           to="/modules"
-          style={isClicked[1] ? bkstyle : {}}
-          onClick={() => {
-            setIsClicked(
-              isClicked[1] ? [false, false, false] : [false, true, false]
-            );
-          }}
+          style={segments[1] === "modules" ? bkstyle : {}}
+          // onClick={() => {
+          //   setIsClicked(
+          //     isClicked[1] ? [false, false, false] : [false, true, false]
+          //   );
+          // }}
           // onMouseEnter={() => setIsDropdownVisible(true)}
           // onMouseLeave={() => setIsDropdownVisible(false)}
         >
@@ -56,12 +60,12 @@ const Navbar = () => {
         <Link
           class="tab link-3"
           to="/about"
-          style={isClicked[2] ? bkstyle : {}}
-          onClick={() => {
-            setIsClicked(
-              isClicked[2] ? [false, false, false] : [false, false, true]
-            );
-          }}
+          style={segments[1] === "about" ? bkstyle : {}}
+          // onClick={() => {
+          //   setIsClicked(
+          //     isClicked[2] ? [false, false, false] : [false, false, true]
+          //   );
+          // }}
         >
           About
         </Link>
